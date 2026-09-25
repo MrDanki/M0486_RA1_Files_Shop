700680a82f805e03de119c55a59aa440550f38e8
Daniel Lop <daniellopbarbara@gmail.com>
2026-09-25 17:12:09 +0200

    Updated DaoFIle

--- src/dao/DaoFile.java
+++ src/dao/DaoFile.java
@@ -1,5 +1,28 @@
 package dao;
 
-public class DaoFile {
+import model.Employee;
+import model.Product;
+import main.Shop;
+
+import java.util.ArrayList;
+
+public class DaoFile implements Dao {
+
+    public void connect() {System.out.println("Opening file...");}
+    public void disconnect() {System.out.println("Closing file...");}
+    public Employee getEmployee(int employeeId,String password) {
+        System.out.println("Getting employee from file...");
+        return null;
+    }
+
+    String filePathInventory = "src/data/inputInventory.txt";
+
+    public ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<>();
+        System.out.println("Getting inventory from file...");
+        // Read the file and populate the inventory list
+        // For now, we will just return an empty list
+        return inventory;
+    }
     
 }
